742379f30ab3377b2698b8747be2cf155e8bee7e
PJ Clinton <github@intheory.bulc.club>
2022-10-26 18:12:51 -0500

    Initalization with AppBar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <!-- Pasted code from vuetify -->
+      <v-app-bar
+          app
+          color="primary"
+          dark
+          >
+          <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+          <v-toolbar-title>This is our kickass app.</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      text
+      link
+      to="/about"
+    >
+      <span class="mr-2">About This Shit</span>
+    </v-btn>
+      </v-app-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-bar />
+</template>
+
+<script>
+  import AppBar from '../components/AppBar'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      AppBar,
+    },
+  }
+</script>
