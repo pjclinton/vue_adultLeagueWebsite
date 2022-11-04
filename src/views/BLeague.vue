1ae534f10b2f743cfd1449f7dca2dfa235e720f6
besampson0 <bnicerrr@gmail.com>
2022-11-04 09:04:55 -0500

    add tables for roster and teams

--- src/views/BLeague.vue
+++ src/views/BLeague.vue
@@ -1,28 +1,61 @@
 <template>
-  <div v-if="$store.state.teams">
-    <ul v-for="team in $store.state.teams" :key="team.id">
-      <router-link to="/roster">
-        <li @click="handleRosterSel(team.teamid)">
-        {{team.teamname}}
-      </li>
-      </router-link>
-  
-    </ul>
+  <div class="container" v-if="$store.state.teams">
+  <v-card>
+    <v-card-title>
+      B League
+    </v-card-title>
+    <v-card-subtitle>
+      2022-2023 Season
+    </v-card-subtitle>
+    <v-card-text>
+      <v-data-table
+        :headers="headers"
+        :items="$store.state.teams"
+        class="elevation-1"
+        :loading="$store.state.isLoading"
+        loading-text="Loading... Please wait"
+        :hide-default-footer="true"
+        elevation="0"
+      >
+      <template v-slot:[`item.teamid`]="{ item }">
+        <v-btn small @click="handleRosterSel(item.teamid)">
+          View Roster
+        </v-btn>
+      </template>
+    </v-data-table>
+    </v-card-text>
+  </v-card>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        headers: [
+        { text: 'Team Name', value: 'teamname', width: '150px' },
+        { text: 'Points', value: 'points', width: '100px' },
+        { text: 'Wins', value: 'totalwins', width: '100px' },
+        { text: 'Losses', value: 'losses', width: '100px' },
+        { text: 'OT Losses', value: 'otl', width: '100px' },
+        { text: 'Ties', value: 'ties', width: '100px'},
+        { text: 'Last Five', value: 'lastfive', width: '100px'},
+        { text: '', value: 'teamid', width: '100px'},
+        ]
+      }
+    },
     methods: {
       handleRosterSel(teamId) {
         this.$store.state.roster = []
         this.$store.dispatch('getRoster', teamId)
-        
+        this.$router.push('/roster')
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  padding: 4rem;
+}
 </style>
