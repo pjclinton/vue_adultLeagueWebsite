04b38d7c27387b8391319aa913545dfeec168cfa
PJ Clinton <github@intheory.bulc.club>
2022-11-10 19:52:47 -0600

    Minor changes

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,50 @@
   <v-app>
     <app-bar />
     <v-main>
-      <Transition name="fade"  mode="out-in">
-      <router-view/>
-    </Transition>
+      <Transition name="fade" mode="out-in">
+        <router-view />
+      </Transition>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import AppBar from '@/components/AppBar.vue';
+import AppBar from "@/components/AppBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     AppBar,
   },
 
   data: () => ({
-    //
   }),
   async created() {
-    this.$store.dispatch('getLeagues')
-  }
+    this.$store.dispatch("getLeagues");
+  },
 };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Exo:wght@100;700&family=Overpass:wght@100;400;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Exo:wght@100;700&family=Inter+Tight&family=Overpass:wght@100;400;900&display=swap");
 #app {
-  background: rgb(4,30,66);
-background: linear-gradient(146deg, rgba(4,30,66,1) 0%, rgba(200,16,46,1) 100%);
-color: #fff;
-  font-family: 'Overpass', sans-serif;
-
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    146deg,
+    rgba(0, 32, 91, 1) 40%,
+    rgba(191, 13, 62, 1) 100%
+  );
+  color: #fff;
+  font-family: "Inter Tight", sans-serif;
+  letter-spacing: .05em;
 }
-.fade-enter-active, .fade-leave-active{
+.fade-enter-active,
+.fade-leave-active {
   transition: all 0.4s ease-out;
 }
 
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   transform: translateX(20px);
   opacity: 0;
 }
