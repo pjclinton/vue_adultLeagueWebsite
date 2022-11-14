3174f1b7bcf7356d766b78ca73442da85c0c4f13
besampson0 <bnicerrr@gmail.com>
2022-11-14 15:56:39 -0600

    update firebase.js file

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -4,7 +4,12 @@ import { getAuth } from "firebase/auth";
 
 
 const config = {
-
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_ID,
+  appId: process.env.VUE_APP_APP_ID,
 };
 
 const app = initializeApp(config);
