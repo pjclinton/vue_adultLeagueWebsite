11314e264a3fe9aee76575b8b459e6e478333f37
besampson0 <bnicerrr@gmail.com>
2022-11-15 09:02:52 -0600

    little fixes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { auth } from "./plugins/firebase";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      vuetify,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
